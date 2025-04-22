59f7c8a71f473f34b4228fabb0b7edb10d1e1815
Glitch (glitch-hello-node) <none>
2025-04-22 21:40:15 +0000

    💗🐘 Checkpoint ./server.js:58438533/5842

--- server.js
+++ server.js
@@ -1,29 +1,133 @@
-const express = require("express");
-const multer = require("multer");
-const xrpl = require("xrpl");
-const { NFTStorage, File } = require("nft.storage");
-const mime = require("mime");
-const fs = require("fs");
-const path = require("path");
-require("dotenv").config();
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-const upload = multer({ dest: "uploads/" });
-
-app.use(express.json());
-
-app.use(require("cors")({
-  origin: [
-    "https://bidds.com",
-    "https://xrp.cafe",
-    "https://sglcn.art",
-    "https://outgoing-destiny-bladder.glitch.me"
-  ],
-  credentials: false
-}));
-
-const nftStorage = new NFTStorage({ token: process.env.NFT_STORAGE_KEY });
-
-const SERVICE_WALLET = "rHN78EpNHLDtY6whT89WsZ6mMoTm9XPi5U";
-const S
+{
+  "swagger": "2.0",
+  "info": {
+    "title": "SGLCN-X20 Minting API",
+    "version": "1.0.0",
+    "description": "This API allows minting of NFTs using SeagullCoin (SGLCN-X20)."
+  },
+  "paths": {
+    "/mint": {
+      "post": {
+        "summary": "Mint an NFT",
+        "description": "Mint an NFT by uploading an image and providing metadata.",
+        "parameters": [
+          {
+            "name": "file",
+            "in": "formData",
+            "description": "The file to upload (image for NFT)",
+            "required": true,
+            "type": "file"
+          },
+          {
+            "name": "name",
+            "in": "formData",
+            "description": "Name of the NFT",
+            "required": true,
+            "type": "string"
+          },
+          {
+            "name": "description",
+            "in": "formData",
+            "description": "Description of the NFT",
+            "required": true,
+            "type": "string"
+          },
+          {
+            "name": "domain",
+            "in": "formData",
+            "description": "Domain of the NFT creator",
+            "required": true,
+            "type": "string"
+          },
+          {
+            "name": "collectionName",
+            "in": "formData",
+            "description": "Collection name for the NFT",
+            "required": true,
+            "type": "string"
+          },
+          {
+            "name": "collectionIcon",
+            "in": "formData",
+            "description": "Icon for the NFT collection",
+            "required": true,
+            "type": "string"
+          },
+          {
+            "name": "properties",
+            "in": "formData",
+            "description": "Additional properties of the NFT",
+            "required": false,
+            "type": "string"
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "NFT minted successfully",
+            "schema": {
+              "type": "object",
+              "properties": {
+                "success": { "type": "boolean" },
+                "tokenId": { "type": "string" }
+              }
+            }
+          },
+          "500": {
+            "description": "Minting failed"
+          }
+        }
+      }
+    },
+    "/nfts": {
+      "get": {
+        "summary": "Get all minted NFTs and collections",
+        "description": "Returns all minted NFTs and their collections.",
+        "responses": {
+          "200": {
+            "description": "List of NFTs and collections",
+            "schema": {
+              "type": "object",
+              "properties": {
+                "mintedNFTs": {
+                  "type": "array",
+                  "items": {
+                    "type": "object",
+                    "properties": {
+                      "name": { "type": "string" },
+                      "description": { "type": "string" },
+                      "image": { "type": "string" },
+                      "tokenId": { "type": "string" },
+                      "collection": { "type": "string" }
+                    }
+                  }
+                },
+                "collections": {
+                  "type": "object",
+                  "additionalProperties": {
+                    "type": "object",
+                    "properties": {
+                      "name": { "type": "string" },
+                      "icon": { "type": "string" },
+                      "nfts": {
+                        "type": "array",
+                        "items": {
+                          "type": "object",
+                          "properties": {
+                            "name": { "type": "string" },
+                            "description": { "type": "string" },
+                            "image": { "type": "string" },
+                            "tokenId": { "type": "string" }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}

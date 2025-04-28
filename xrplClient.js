9a4c4e3dd4dba52b05d42188ec8cfe37f8271610
Glitch (sglcn-x20-api) <none>
2025-04-28 15:05:36 +0000

    🏚👾 Checkpoint ./xrplClient.js:58438533/8 ./mintingLogic.js:58438533/54938 ./server.js:58438533/14248

--- xrplClient.js
+++ xrplClient.js
@@ -1,7 +1,7 @@
 // xrplClient.js
 import xrpl from 'xrpl';
 
-const client = new xrpl.Client('https://s.altnet.rippletest.net:51234/');
+const client = new xrpl.Client('wss://s.altnet.rippletest.net:51234/');
 
 async function connectClient() {
   if (!client.isConnected()) {

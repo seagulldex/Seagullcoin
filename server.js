daddb8ae69ae72cb6b1f0d1dbdb526168bd1d29f
Glitch (sglcn-x20-api) <none>
2025-05-01 21:41:45 +0000

    🏯🍂 Checkpoint ./server.js:58438533/208451

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ const port = process.env.PORT || 3000;
 const myCache = new NodeCache({ stdTTL: 600, checkperiod: 120 });
 const { XUMM_CLIENT_ID, XUMM_CLIENT_SECRET, XUMM_REDIRECT_URI, SGLCN_ISSUER, SERVICE_WALLET } = process.env;
 const db = new sqlite3.Database('./database.db');
+const nftStorage = new NFTStorage({ token: process.env.NFT_STORAGE_API_KEY });
 
 export { getTotalNFTs, getMostLikedNFTs, getTotalUsers, getTotalMints };
 

0e2eb26142a68d82b1e4c92723e209747faae571
Glitch (sglcn-x20-api) <none>
2025-05-04 21:15:16 +0000

    👯🕌 Checkpoint ./server.js:58438533/91043 ./mint-endpoint.js:58438533/8876

--- server.js
+++ server.js
@@ -1514,4 +1514,4 @@ setInterval(cleanupExpiredPayloads, 24 * 60 * 60 * 1000); // Every 24 hours
 // Start the server
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server running on port ' + (process.env.PORT || 3000));
-}); 
+});  

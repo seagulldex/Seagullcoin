94389dad8b264a5a515bcb58d9f743fed0ccc03f
seagulldex <bseagullclub@gmail.com>
2025-06-16 12:53:42 -0700

    Update server.js

--- server.js
+++ server.js
@@ -70,6 +70,7 @@ import { RippleAPI } from 'ripple-lib';
 import { Client } from 'xrpl';
 import { fetchSeagullOffers } from "./offers.js";
 import Stripe from 'stripe';
+import nodemailer from 'nodemailer';
 
 // ===== Init App and Env =====
 dotenv.config();

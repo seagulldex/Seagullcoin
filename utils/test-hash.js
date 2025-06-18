f3917515c95009c5b31848661e988e52eadf24d0
seagulldex <bseagullclub@gmail.com>
2025-06-18 10:19:16 -0700

    Update test-hash.js

--- utils/test-hash.js
+++ utils/test-hash.js
@@ -3,10 +3,3 @@ import crypto from 'crypto';
 export function hashSeed(seed) {
   return crypto.createHash('sha256').update(seed).digest('hex');
 }
-
-// Test example
-if (import.meta.url === `file://${process.argv[1]}`) {
-  const testSeed = "example";
-  console.log("Seed:", testSeed);
-  console.log("Hashed:", hashSeed(testSeed));
-}

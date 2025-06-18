ecd213fd00988ccce4749bd306d2e8edabfc12cd
seagulldex <bseagullclub@gmail.com>
2025-06-18 13:38:49 -0700

    Create UserWallet.js

---
+++ models/UserWallet.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const WalletSchema = new mongoose.Schema({
+  wallet: { type: String, required: true, unique: true },
+  seed: { type: String, required: false },            // optional, ideally don’t save plain seed
+  hashed_seed: { type: String, required: true },      // hashed seed should be required
+  xrpl_address: { type: String, required: false },
+  xumm_uuid: { type: String, required: false },
+}, { timestamps: true });
+
+const Wallet = mongoose.model('UserWallet', WalletSchema);
+export default Wallet;

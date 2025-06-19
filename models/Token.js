b2fe33db2233797dd98701f491a597b81e4c0728
seagulldex <bseagullclub@gmail.com>
2025-06-18 19:24:38 -0700

    Update Token.js

--- models/Token.js
+++ models/Token.js
@@ -19,15 +19,19 @@ const tokenSchema = new mongoose.Schema({
   },
   max_supply: {
     type: Number,
-    required: true
+    required: true,
+    min: 0
   },
   circulating_supply: {
     type: Number,
-    default: 0
+    default: 0,
+    min: 0
   },
   decimals: {
     type: Number,
-    default: 0
+    default: 0,
+    min: 0,
+    max: 18
   },
   isGenesisToken: {
     type: Boolean,
@@ -37,11 +41,10 @@ const tokenSchema = new mongoose.Schema({
     type: String,
     enum: ['L1', 'L2'],
     default: 'L2'
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
-});
+    }
+  }, { timestamps: true });
+
+// Index for common token queries per wallet
+tokenSchema.index({ owner_wallet: 1, symbol: 1 });
 
 export default mongoose.model('Token', tokenSchema);

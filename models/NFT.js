5006cafe5471e06cc30694f295cbddcf6079fa5e
seagulldex <bseagullclub@gmail.com>
2025-06-20 00:09:36 -0700

    Update NFT.js

--- models/NFT.js
+++ models/NFT.js
@@ -4,15 +4,15 @@ const nftSchema = new mongoose.Schema({
   wallet: String,
   NFTokenID: String,
   URI: String,
-  icon: String,
-  collection: Object,
-  metadata: Object,
   image: String,
   name: String,
   traits: Array,
+  collection: String,
+  icon: String,
+  metadata: Object,
   updatedAt: { type: Date, default: Date.now },
 });
 
 nftSchema.index({ wallet: 1, NFTokenID: 1 }, { unique: true });
 
-export default mongoose.models.NFT || mongoose.model('NFT', nftSchema);
+export const NFTModel = mongoose.models.NFT || mongoose.model('NFT', nftSchema);

2739a497f1a6db57c44d8fa167024bec25cd7378
seagulldex <bseagullclub@gmail.com>
2025-06-22 12:05:32 -0700

    Update Block.js

--- models/Block.js
+++ models/Block.js
@@ -6,7 +6,9 @@ const BlockSchema = new mongoose.Schema({
   timestamp: { type: Date, default: Date.now },
   transactions: { type: [Object], default: [] },
   nonce: { type: Number, default: 0 },
-  hash: { type: String, required: true }
+  hash: { type: String, required: true },
+  tokenName: { type: String, default: null },   // add this
+  tokenSymbol: { type: String, default: null }, // add this
 });
 
 export default mongoose.model('Block', BlockSchema);

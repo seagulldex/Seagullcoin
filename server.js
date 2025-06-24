7d8ebc5f2ce5377794ef6269736376ee07519465
seagulldex <bseagullclub@gmail.com>
2025-06-23 20:14:24 -0700

    Update server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ import PendingTransaction from './models/PendingTransaction.js';
 import ValidatorNode from './models/ValidatorNode.js';
 import { signBlock } from './utils/signBlock.js'; // existing
 import { verifySignature } from './utils/verifySignature.js';
-import React, { useEffect, useState } from 'react';
+
 
 // ===== Init App and Env =====
 dotenv.config();

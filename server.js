9fba61b8c99f69f90659f68263c5ebf97575f4ca
seagulldex <bseagullclub@gmail.com>
2025-06-24 18:58:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5875,7 +5875,7 @@ app.post('/mine', async (req, res) => {
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'explorer.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 

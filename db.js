6ac0a095fe69b0f39419010ecc5923cdc4cffb2a
seagulldex <bseagullclub@gmail.com>
2025-06-27 10:20:36 -0700

    Create db.js

---
+++ db.js
@@ -0,0 +1,8 @@
+// db.ts
+import mongoose from 'mongoose';
+import { Token } from './models/Token'; // your Mongoose schema
+
+export async function loadGenesisToken() {
+  const token = await Token.findOne({ isGenesisToken: true });
+  return token;
+}

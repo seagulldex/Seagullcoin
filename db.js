a5e637319cc5d9d2c25940453544e4c58efa0f13
seagulldex <bseagullclub@gmail.com>
2025-06-28 07:10:01 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 // db.ts
 import mongoose from 'mongoose';
-import { Token } from '../models/Token'; // your Mongoose schema
+import { Token } from './models/Token'; // your Mongoose schema
 
 export async function loadGenesisToken() {
   const token = await Token.findOne({ isGenesisToken: true });

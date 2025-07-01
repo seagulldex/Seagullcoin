7df08ea72eac9512eab2d9581ba4260b7d02e7ad
seagulldex <bseagullclub@gmail.com>
2025-07-01 16:32:47 -0700

    Update SGLCNXAUPrice.js

--- models/SGLCNXAUPrice.js
+++ models/SGLCNXAUPrice.js
@@ -6,4 +6,10 @@ const SGLCNXAUPriceSchema = new mongoose.Schema({
   xau_to_sglcn: Number
 });
 
+// For faster queries on recent data
+SGLCNXAUPriceSchema.index({ timestamp: -1 });
+
+// Optional auto-expiry after 30 days
+// SGLCNXAUPriceSchema.index({ timestamp: 1 }, { expireAfterSeconds: 60 * 60 * 24 * 30 });
+
 export default mongoose.model('SGLCNXAUPrice', SGLCNXAUPriceSchema);

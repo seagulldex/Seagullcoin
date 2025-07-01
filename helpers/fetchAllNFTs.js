7b44b5363643df945944c05eee8443b65598a8b8
seagulldex <bseagullclub@gmail.com>
2025-07-01 00:08:17 -0700

    Update fetchAllNFTs.js

--- helpers/fetchAllNFTs.js
+++ helpers/fetchAllNFTs.js
@@ -1,5 +1,5 @@
 const nftCache = new Map();
-const xrplApiUrl = 'https://xrplcluster.com;
+const xrplApiUrl = 'https://xrplcluster.com';
 
 
 export const fetchAllNFTs = async (wallet, nftCache, xrplApiUrl) => {

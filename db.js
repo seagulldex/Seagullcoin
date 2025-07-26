4415658a51b1e56f68f682433e25bea3c19e8705
seagulldex <bseagullclub@gmail.com>
2025-07-26 08:48:29 -0700

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,11 @@
 import mongoose from 'mongoose';
 import Token from './models/Token.js';  // default import
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/gossipDB';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://seagullcoin-dex-uaj3x.ondigitalocean.app/gossipDB';
 
 export async function connectDB() {
   if (mongoose.connection.readyState === 0) {
+    dbName: 'gossipDB',
     await mongoose.connect(MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,

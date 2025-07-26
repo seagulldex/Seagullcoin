868897e922b314cefd976f11ed24a8f5c4f8455c
seagulldex <bseagullclub@gmail.com>
2025-07-25 17:57:37 -0700

    Update Iso20022.js

--- models/Iso20022.js
+++ models/Iso20022.js
@@ -2,15 +2,14 @@ import mongoose from 'mongoose';
 
 const Iso20022Schema = new mongoose.Schema({
   xrpl_address: { type: String, required: true },
-  wallet: { type: String, required: false, default: null },
-  xlm_address: { type: String, required: false, default: null },
-  flr_address: { type: String, required: false, default: null },
-  hbar_address: { type: String, required: false, default: null },
-  algo_address: { type: String, required: false, default: null },
-  xdc_address: { type: String, required: false, default: null },
+  wallet: { type: String, default: null },
+  xlm_address: { type: String, default: null },
+  flr_address: { type: String, default: null },
+  hbar_address: { type: String, default: null },
+  algo_address: { type: String, default: null },
+  xdc_address: { type: String, default: null },
   timestamp: { type: Date, default: Date.now }
 });
-
 
 const Iso20022 = mongoose.models.Iso20022 || mongoose.model('Iso20022', Iso20022Schema);
 export default Iso20022;

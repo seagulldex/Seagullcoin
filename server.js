ddfc7685ba340ceef488038016a53e2c7b139a2b
seagulldex <bseagullclub@gmail.com>
2025-07-26 08:45:24 -0700

    Update server.js

--- server.js
+++ server.js
@@ -245,15 +245,6 @@ async function createStakePayload(req, res, amount) {
 
 
 
-const connectDB = async () => {
-  if (mongoose.connection.readyState === 1) return; // already connected
-  await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
-};
-
-
 
 // ⏰ Cleanup logic
 async function cleanOldPendingStakes() {

c32b8f60f87d46826ac4650e319a35a813a49600
seagulldex <bseagullclub@gmail.com>
2025-07-31 19:34:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ import Iso20022 from './models/Iso20022.js';  // adjust the path
 import BridgeRequest from "./models/BridgeRequest.js";
 import { basicAuth } from './authMiddleware.js';
 import { AssetRegistry, IsoMessage, BridgeHistory } from './models/index.js';
-import parseString from 'xml2js';
+import { parseString } from 'xml2js';
 
 
 dotenv.config();
